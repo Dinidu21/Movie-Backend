4a331ed89b82b38aa2846a1a6458689a3b014f51
Dinidu Sachintha <dinidusachintha3@gmail.com>
2025-01-01 20:59:32 +0530

    Setup Env 2025-01-01 20-59-32 @Dinidu21

--- src/main/java/dev/dinidu/Movies/MoviesApplication.java
+++ src/main/java/dev/dinidu/Movies/MoviesApplication.java
@@ -2,13 +2,12 @@ package dev.dinidu.Movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class MoviesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MoviesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MoviesApplication.class, args);
+    }
 
 }
