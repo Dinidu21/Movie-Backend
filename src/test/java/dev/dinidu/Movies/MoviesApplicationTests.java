fe9251ac6550250286b69a98cf89373283cbd7b6
Dinidu Sachintha <dinidusachintha3@gmail.com>
2025-01-01 20:16:42 +0530

    first commit

---
+++ src/test/java/dev/dinidu/Movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dinidu.Movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
